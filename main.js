fcde4bb0441d826105edcf060f87f6d013af7a7e
Samuel Jacobs <jacobssamuel57@gmail.com>
2021-12-30 12:51:26 +0100

    new commit

--- main.js
+++ main.js
@@ -29,58 +29,6 @@ function showSlides(n) {
 }
 
 
-//second carousel
-let slideIndexs = 1;
-displaySlides(slideIndexs);
-
-function currentSlides(n) {
-  displaySlides(slideIndexs = n);
-}
-
-function displaySlides(n) {
-  var i;
-
-  var slides = document.getElementsByClassName("mySlides1");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndexs = 1}
-  if (n < 1) {slideIndexs = slides.length }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndexs-1].style.display = "block";
-  dots[slideIndexs-1].className += " active";
-
-}
-
-//third carousel
-let slidesIndex = 1;
-revealSlides(slidesIndex);
-
-function activeSlide(n) {
-  revealSlides(slidesIndex = n);
-}
-
-function revealSlides(n) {
-  var i;
-
-  var slides = document.getElementsByClassName("mySlides2");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slidesIndex = 1}
-  if (n < 1) {slidesIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slidesIndex-1].style.display = "block";
-  dots[slidesIndex-1].className += " active";
-
-}
-
 
 /* Open when someone clicks on the span element */
 function openNav() {
